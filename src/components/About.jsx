a3dcc62ef27b9798cba3cca636f3eda2b5b4a1bd
akibahmed229 <akib4418@gmail.com>
2023-06-02 04:01:30 +0600

    Some changes on ui

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,17 +10,17 @@ const About = ({ darkMode }) => {
       photo: "/images/Ahmed-logo.jpg",
     },
     {
-      name: "Pollob Chondro Dash",
+      name: "Pallab Chandra Das",
       role: "Fronted Developer",
       photo: "/images/pollob.png",
     },
     {
-      name: "Himel",
+      name: "Hemel",
       role: "UI/UX Designer",
       photo: "/images/Hemel.jpg",
     },
     {
-      name: "Nahid",
+      name: "MD Nahidul Islam",
       role: "UI/UX Designer",
       photo: "/images/nahidjpg.jpg",
     },
@@ -35,26 +35,28 @@ const About = ({ darkMode }) => {
   }, []);
 
   return (
-    <div
-      className="flex flex-col items-center justify-center min-h-screen"
-      ref={topRef}
-    >
-      <h1 className="text-4xl font-bold mb-8 mt-8">
-        About Employee Management System
-      </h1>
-      <div className="rounded-lg shadow-2xl p-6">
-        <h2 className="text-2xl font-bold mb-4">Overview</h2>
-        <p className="text-white">
-          The Employee Management System is a Java-based application that
-          provides a user-friendly interface for managing employee details.
-        </p>
-        <h2 className="text-2xl font-bold mt-6 mb-4">Features</h2>
-        <ul className="list-disc ml-8">
-          <li>Perform CRUD operations on employee data</li>
-          <li>Search, view, update, and remove employee records</li>
-          <li>Store employee data using the OS file system</li>
-          <li>Utilize Java AWT &amp; Swing for the graphical interface</li>
-        </ul>
+    <div className="flex flex-col items-center justify-center " ref={topRef}>
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold mb-8 mt-8">
+          About Employee Management System
+        </h1>
+        <div className="rounded-lg shadow-2xl p-6 ">
+          <h2 className="text-2xl font-bold mb-4">Overview</h2>
+          <p className="text-white">
+            The Employee Management System is a Java-based application that
+            provides a user-friendly interface for managing employee details.
+          </p>
+          <h2 className="text-2xl font-bold mt-6 mb-4">Features</h2>
+          <ul className="list-disc ml-8">
+            <li>Perform CRUD operations on employee data</li>
+            <li>Search, view, update, and remove employee records</li>
+            <li>Store employee data using the OS file system</li>
+            <li>Utilize Java AWT &amp; Swing for the graphical interface</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
         <h2 className="text-2xl font-bold mt-6 mb-4">Developers</h2>
 
         <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
@@ -83,12 +85,14 @@ const About = ({ darkMode }) => {
             </div>
           ))}
         </div>
-      </div>
 
-      <p className={`${darkMode} ? "text-white" : "text-gray-700"} mt-4 mb-8`}>
-        The Employee Management System simplifies the process of managing
-        employee information and ensures efficient data storage and retrieval.
-      </p>
+        <p
+          className={`${darkMode} ? "text-white" : "text-gray-700"} mt-4 mb-8`}
+        >
+          The Employee Management System simplifies the process of managing
+          employee information and ensures efficient data storage and retrieval.
+        </p>
+      </div>
     </div>
   );
 };
