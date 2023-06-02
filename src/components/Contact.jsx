00ab75f7d8e72cddf1b3c1a9dadc86f432d93db4
akibahmed229 <akib4418@gmail.com>
2023-06-02 14:53:25 +0600

    Changes on Ui

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,53 +13,104 @@ const Contact = ({ darkMode }) => {
 
   return (
     <div
-      className={`flex flex-col items-center justify-center h-screen  mx-auto`}
+      className={`flex flex-col items-center justify-center   mx-auto`}
       ref={topRef}
     >
-      <div
-        className={`max-w-6xl p-8 ${
-          darkMode ? "bg-[#1f2937]" : "bg-white"
-        }  shadow-xl rounded-lg`}
-      >
-        <h1 className="text-2xl font-bold md:mb-6 lg:mb-6 xl:mb-6">
-          Contract Services
-        </h1>
-        <p className={` mb-6 ${darkMode ? "text-white" : "text-gray-600"} `}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          cursus quam ac justo fringilla, in volutpat lacus pharetra. Aenean ac
-          turpis enim.
-        </p>
-
-        <div className="mb-6">
-          <h2 className="text-lg font-bold mb-2">Service Details</h2>
-          <ul
-            className={`list-disc list-inside ${
+      <div className="h-screen  flex flex-col justify-center items-center">
+        <div
+          className={`max-w-6xl p-8 ${
+            darkMode ? "bg-[#1f2937]" : "bg-white"
+          }  shadow-xl rounded-lg`}
+        >
+          <h1 className="text-2xl font-bold md:mb-6 lg:mb-6 xl:mb-6">
+            Contract Services
+          </h1>
+          <p
+            className={` mb-6 text-m ${
               darkMode ? "text-white" : "text-gray-600"
             } `}
           >
-            <li>Lorem ipsum dolor sit amet</li>
-            <li>Consectetur adipiscing elit</li>
-            <li>Nulla facilisi</li>
-            <li>Etiam sed diam eu nisl pretium</li>
-          </ul>
-        </div>
+            Our experienced team provides high-quality solutions in areas such
+            as legal, financial, consulting, and more. With a focus on contract
+            drafting, review, negotiation, and management, we ensure legally
+            sound agreements that protect your interests. Our personalized
+            approach and industry expertise simplify complex matters and deliver
+            effective results. Trust us to handle your contract needs while you
+            concentrate on your core business objectives. Contact us today to
+            learn how our comprehensive services can help you achieve your
+            goals.
+          </p>
 
-        <div>
-          <h2 className="text-lg font-bold mb-2">Contact Us</h2>
-          <p className={` mb-2 ${darkMode ? "text-white" : "text-gray-600"} `}>
-            If you have any questions or would like to request our contract
-            services, please fill out the form below or reach out to us:
-          </p>
-          <p className={`${darkMode ? "text-white" : "text-gray-600"} `}>
-            Phone: <span className="text-blue-500">+1 (123) 456-7890</span>
-          </p>
-          <p className={`${darkMode ? "text-white" : "text-gray-600"} `}>
-            Email:{" "}
-            <span className="text-blue-500">info@contractservices.com</span>
-          </p>
-        </div>
+          <div className="mb-6">
+            <h2 className="text-lg font-bold mb-2">Service Details</h2>
+            <ul
+              className={`list-disc list-inside text-sm space-y-4 ${
+                darkMode ? "text-white" : "text-gray-600"
+              } `}
+            >
+              <li>
+                User-Friendly Interface: Our site offers a user-friendly
+                interface that allows visitors to navigate effortlessly and
+                access different sections of the Employee Management system.
+              </li>
 
-        <form className="mt-8">
+              <li>
+                Project Overview: We provide a detailed overview of the Java
+                Employee Management project, explaining its purpose,
+                functionalities, and benefits. Visitors can gain a clear
+                understanding of how the system streamlines employee management
+                tasks.
+              </li>
+
+              <li>
+                Screenshots and Demo: Through screenshots and a live demo, we
+                showcase the system's user interface, demonstrating its
+                intuitive design and functionality. Visitors can visually
+                experience the system and its features in action.
+              </li>
+              <li>
+                Feature Highlights: We highlight the key features of the
+                Employee Management system, such as employee search, data
+                update, record addition, and deletion. This helps visitors
+                understand how the system simplifies and enhances the management
+                of employee details.
+              </li>
+              <li>
+                Technology Stack: We provide information about the technologies
+                used in developing the Java Employee Management project,
+                including Java, AWT, Swing, and the OS file system. This gives
+                visitors insights into the technical aspects of the system.
+              </li>
+              <li>
+                Contact and Support: We offer a contact form for visitors to
+                reach out to us with any inquiries or support requests related
+                to the Employee Management system. Our team is readily available
+                to provide assistance and address any questions or concerns.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`${
+          darkMode ? "bg-[#1f2937]" : "bg-white"
+        } w-[60vw]  p-8  shadow-xl rounded-lg  m-20`}
+      >
+        <h2 className="text-lg font-bold mb-2">Contact Us</h2>
+        <p className={` mb-2 ${darkMode ? "text-white" : "text-gray-600"} `}>
+          If you have any questions or would like to request our contract
+          services, please fill out the form below or reach out to us:
+        </p>
+        <p className={`${darkMode ? "text-white" : "text-gray-600"} `}>
+          Phone: <span className="text-blue-500">+1 (123) 456-7890</span>
+        </p>
+        <p className={`${darkMode ? "text-white" : "text-gray-600"} `}>
+          Email:{" "}
+          <span className="text-blue-500">info@contractservices.com</span>
+        </p>
+
+        <form className="mt-8 p-8">
           <div className="mb-4">
             <label
               htmlFor="name"
